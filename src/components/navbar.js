b51a45a3f969ad9eb3113e8bd0fa67fcbfceef0d
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2020-12-07 11:02:41 +0200

    Tailwindcss installed

---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const navbar = () => {
+  return (
+    <div>
+      <head>
+      </head>
+
+      <body>
+
+	<nav class="navbar">
+		<ul class="navbar-new">
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+
+					<svg....></svg> // svg icons
+
+					<span class="link-text">About</span>
+
+			</li>
+		</ul>
+	</nav>
+  </body>
+
+	<main>
+		<h1>This is a heading</h1>
+		<p>something something paragraph text</p>
+	</main>
+    </div>
+  )
+}
