af9047f57ab21da2a25df27ba21ad4719cbed907
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-02-19 20:47:53 +0200

    ErgoTimer project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import DesignSystemsDark from "../images/DesignSystemsDark.png";
 import DesignSystemsLight from "../images/DesignSystemsLight.png";
+import ErgoTimer from "../images/ErgoTimer.png";
 import { FaGithub, FaCodepen, FaLinkedinIn, FaDribbble } from "react-icons/fa";
 
 const Projects = () => {
@@ -18,8 +19,8 @@ const Projects = () => {
           </h1>
         </div>
         <hr className="border-1 border-bottom border-gray-600 w-full mt-8 mb-10"></hr>
-        <div className="project-container pt-20 grid grid-cols-2">
-          <Link className="project-image relative grid grid-cols-2 hover:opacity-60">
+        <div className="project-container-right pt-32 grid grid-cols-2">
+          <Link className="project-image relative grid grid-cols-2">
             <img
               className="image grid-span-2 grid-start-1"
               src={DesignSystemsLight}
@@ -30,7 +31,7 @@ const Projects = () => {
               src={DesignSystemsDark}
               alt="DesignSystemsDark"
             />
-            <Link className="project-link m-auto my-auto opacity-0 text-gray-100 bg-gray-800 rounded p-2 px-4">
+            <Link className="project-link m-auto my-auto opacity-0 text-gray-400 focus:text-gray-100 focus:ring-1 focus:ring-gray-100 bg-gray-800 rounded px-6 py-2.5 border-gray-400 focus:border-gray-100 border-2 shadow">
               view project
             </Link>
           </Link>
@@ -68,6 +69,52 @@ const Projects = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="project-container-left pt-60 grid grid-cols-2">
+          <div className="project-info text-left">
+            <h2 className="text-red-300 text-5xl font-bold">Ergo Timer</h2>
+            <h3 className="text-gray-300 text-3xl font-bold mt-2">
+              with CSS Variables & Animation
+            </h3>
+            <div className="bg-gray-800 text-left p-4 mt-12 mr-10">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
+                facilisi consequat vestibulum accumsan vitae risus volutpat
+                pharetra. Etiam urna, tortor enim volutpat cursus nullam augue
+                ac. Aenean risus, purus nec massa vitae id at turpis nullam.
+                Tincidunt ullamcorper ac vitae egestas lorem sit justo.
+                Vestibulum, aliquam mauris turpis mi, feugiat.{" "}
+              </p>
+            </div>
+            <ul className="flex row justify-start text-gray-300 mt-8 font-normal">
+              <li>React</li>
+              <li className="pl-6">CSS Variables</li>
+              <li className="pl-6">CSS Animation</li>
+              <li className="pl-6">Figma</li>
+            </ul>
+            <div className="flex row justify-start mt-8 text-gray-300">
+              <div className="icon-primary">
+                <a href="https://github.com/Chrys-Nicolaides">
+                  <FaGithub />
+                </a>
+              </div>
+              <div className="icon-primary">
+                <a href="https://codepen.io/Chrys-Nicolaides">
+                  <FaDribbble />
+                </a>
+              </div>
+            </div>
+          </div>
+          <Link className="project-image relative">
+            <img
+              className="image aspect-h-auto"
+              src={ErgoTimer}
+              alt="ErgoTimer"
+            />
+            <Link className="project-link m-auto my-auto opacity-0 text-gray-400 focus:text-gray-100 focus:ring-1 focus:ring-gray-100 bg-gray-800 rounded px-6 py-2.5 border-gray-400 focus:border-gray-100 border-2 shadow">
+              view project
+            </Link>
+          </Link>
         </div>
       </section>
     </div>
