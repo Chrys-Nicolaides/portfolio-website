c67c3845885e1304e92759986a81d2ddf9f98ea4
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-02-20 21:14:33 +0200

    Individial project components set up

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Link } from "react-router-dom";
 
-import DesignSystemsDark from "../images/DesignSystemsDark.png";
-import DesignSystemsLight from "../images/DesignSystemsLight.png";
-import ErgoTimer from "../images/ErgoTimer.png";
-import { FaGithub, FaCodepen, FaLinkedinIn, FaDribbble } from "react-icons/fa";
+import ProjectOne from "../projects/ProjectOne";
+import ProjectTwo from "../projects/ProjectTwo";
 
 const Projects = () => {
   return (
@@ -19,113 +17,8 @@ const Projects = () => {
           </h1>
         </div>
         <hr className="border-1 border-bottom border-gray-600 w-full mt-8 mb-10"></hr>
-        <div className="project-container-right pt-32 grid grid-cols-2 gap-x-8">
-          <Link className="relative grid grid-cols-2 hover-wrapper">
-            <div className="grid-span-2 grid-start -mr-32">
-            <img
-              className="image"
-              src={DesignSystemsLight}
-              alt="DesignSystemsLight"
-              />
-              </div>
-              <div className="grid-span-2 grid-start-2 z-1 -ml-32 mt-36">
-            <img
-              className="image"
-              src={DesignSystemsDark}
-              alt="DesignSystemsDark"
-              />
-              </div>
-            <div className="absolute inset-0 flex justify-center items-center button-wrapper">
-              <Link className="button-secondary">
-                view project
-              </Link>
-            </div>
-          </Link>
-          <div className="project-info text-right">
-            <h2 className="text-red-300 text-5xl font-bold">Design Systems</h2>
-            <h3 className="text-gray-300 text-3xl font-bold mt-2">
-              with Styled Components & Storybook
-            </h3>
-            <div className="bg-gray-800 text-right p-4 mt-12">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
-                facilisi consequat vestibulum accumsan vitae risus volutpat
-                pharetra. Etiam urna, tortor enim volutpat cursus nullam augue
-                ac. Aenean risus, purus nec massa vitae id at turpis nullam.
-                Tincidunt ullamcorper ac vitae egestas lorem sit justo.
-                Vestibulum, aliquam mauris turpis mi, feugiat.{" "}
-              </p>
-            </div>
-            <ul className="flex row justify-end text-gray-300 mt-8 font-normal">
-              <li>React</li>
-              <li className="pl-6">Styled Components</li>
-              <li className="pl-6">Storybook</li>
-              <li className="pl-6">Figma</li>
-            </ul>
-            <div className="flex row justify-end mt-8 text-gray-300">
-              <div className="icon-primary">
-                <a href="https://github.com/Chrys-Nicolaides">
-                  <FaGithub />
-                </a>
-              </div>
-              <div className="icon-primary">
-                <a href="https://codepen.io/Chrys-Nicolaides">
-                  <FaDribbble />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="project-container-left pb-32 pt-60 grid grid-cols-2">
-          <div className="project-info text-left">
-            <h2 className="text-red-300 text-5xl font-bold">Ergo Timer</h2>
-            <h3 className="text-gray-300 text-3xl font-bold mt-2">
-              with CSS Variables & Animation
-            </h3>
-            <div className="bg-gray-800 text-left p-4 mt-12 mr-10">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
-                facilisi consequat vestibulum accumsan vitae risus volutpat
-                pharetra. Etiam urna, tortor enim volutpat cursus nullam augue
-                ac. Aenean risus, purus nec massa vitae id at turpis nullam.
-                Tincidunt ullamcorper ac vitae egestas lorem sit justo.
-                Vestibulum, aliquam mauris turpis mi, feugiat.{" "}
-              </p>
-            </div>
-            <ul className="flex row justify-start text-gray-300 mt-8 font-normal">
-              <li>React</li>
-              <li className="pl-6">CSS Variables</li>
-              <li className="pl-6">CSS Animation</li>
-              <li className="pl-6">Figma</li>
-            </ul>
-            <div className="flex row justify-start mt-8 text-gray-300">
-              <div className="icon-primary">
-                <a href="https://github.com/Chrys-Nicolaides">
-                  <FaGithub />
-                </a>
-              </div>
-              <div className="icon-primary">
-                <a href="https://codepen.io/Chrys-Nicolaides">
-                  <FaDribbble />
-                </a>
-              </div>
-            </div>
-          </div>
-          <Link className="hover-wrapper relative">
-            <div>
-            <img
-              className="image"
-              src={ErgoTimer}
-              alt="ErgoTimer"
-              />
-              </div>
-            <div className="absolute inset-0 flex justify-center items-center button-wrapper">
-            <Link className="button-secondary">
-              view project
-            </Link>
-          </div>
-          </Link>
-        </div>
+        <ProjectOne />
+        <ProjectTwo />
       </section>
     </div>
   );
---
+++ src/projects/ProjectTwo.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+
+import ErgoTimer from "../images/ErgoTimer.png";
+import { FaGithub, FaDribbble } from "react-icons/fa";
+
+const ProjectTwo = () => {
+  return (
+    <div>
+      <div className="project-container-left pb-32 pt-60 grid grid-cols-2">
+          <div className="project-info text-left">
+            <h2 className="text-red-300 text-5xl font-bold">Ergo Timer</h2>
+            <h3 className="text-gray-300 text-3xl font-bold mt-2">
+              with CSS Variables & Animation
+            </h3>
+            <div className="bg-gray-800 text-left p-4 mt-12 mr-10">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam
+                facilisi consequat vestibulum accumsan vitae risus volutpat
+                pharetra. Etiam urna, tortor enim volutpat cursus nullam augue
+                ac. Aenean risus, purus nec massa vitae id at turpis nullam.
+                Tincidunt ullamcorper ac vitae egestas lorem sit justo.
+                Vestibulum, aliquam mauris turpis mi, feugiat.
+              </p>
+            </div>
+            <ul className="flex row justify-start text-gray-300 mt-8 font-normal">
+              <li>React</li>
+              <li className="pl-6">CSS Variables</li>
+              <li className="pl-6">CSS Animation</li>
+              <li className="pl-6">Figma</li>
+            </ul>
+            <div className="flex row justify-start mt-8 text-gray-300">
+              <div className="icon-primary">
+                <a href="https://github.com/Chrys-Nicolaides">
+                  <FaGithub />
+                </a>
+              </div>
+              <div className="icon-primary">
+                <a href="https://codepen.io/Chrys-Nicolaides">
+                  <FaDribbble />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Link className="hover-wrapper relative">
+            <div>
+             <img
+              className="image"
+              src={ErgoTimer}
+              alt="ErgoTimer"
+              />
+              </div>
+               <div className="absolute inset-0 flex justify-center items-center button-wrapper">
+                <Link className="button-secondary">
+                   view project
+                </Link>
+              </div>
+            </Link>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectTwo;
